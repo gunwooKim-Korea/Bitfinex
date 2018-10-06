507463c018bb5f39acec82202352fa7292fec2a9
Struggler <jnu2345@gmail.com>
2018-10-06 22:26:23 +0900

    Update lender.js

--- lender.js
+++ lender.js
@@ -1,30 +1,115 @@
-url = "https://api.bitfinex.com/v1/";
-const request = require('request')
-request.get(url + '/lendbook/usd',
-  function(error, response, body) {
-    console.log(body);
-})
+const request = require('request');	
+const crypto = require('crypto');
 
-// optionally to limit bids and asks to length of 30:
+function main() {
 
-request.get(url + '/lendbook/usd?limit_bids=30&limit_asks=30',
-  function(error, response, body) {
-    console.log(body);
-})
+	const apiKey = '';
+	const apiSecret = '';
 
-// or use bitfinex-api-node
+	const baseUrl = 'https://api.bitfinex.com';
 
-const BFX = require('bitfinex-api-node')
-const bfxRest = new BFX(apiKey, apiSecretKey, {version: 1}).rest
-bfxRest.fundingbook('USD', (err, res) => {
-	if (err) console.log(err)
-	console.log(res)
-})
+	const url = '/v1/balances';
+	const nonce = Date.now().toString();
+	const completeURL = baseUrl + url;
+	const body = {
+	  request: url,
+	  nonce
+	};
+	const payload = new Buffer(JSON.stringify(body))
+		.toString('base64');
 
-// or use bitfinex-api-node w/ limits
+	const signature = crypto
+	  .createHmac('sha384', apiSecret)
+	  .update(payload)
+	  .digest('hex');
 
-const options = {'limit_asks': 2, 'limit_bids': 2}
-bfxRest.fundingbook('USD', options, (err, res) => {
-	if (err) console.log(err)
-	console.log(res)
-})
+	const options = {
+	  url: completeURL,
+	  headers: {
+		'X-BFX-APIKEY': apiKey,
+		'X-BFX-PAYLOAD': payload,
+		'X-BFX-SIGNATURE': signature
+	  },
+	  body: JSON.stringify(body)
+	};
+
+	return request.post(
+	  options,
+	  function(error, response, body) {
+		console.log('response:', JSON.stringify(body));
+	  }
+	);
+	
+	/*
+	const apiPath = 'v2/auth/r/alerts';
+	const nonce = Date.now().toString();
+	const queryParams = 'type=price';
+	const body = {};
+	let signature = `/api/${apiPath}${nonce}${JSON.stringify(body)}`;
+
+	const sig = crypto.createHmac('sha384', apiSecret).update(signature);
+	const shex = sig.digest('hex');
+
+	const uri_v2 = "https://api.bitfinex.com/v2";
+
+	const options = {
+		url: uri_v2 + "/auth/r/wallets",//`https://api.bitfinex.com/${apiPath}?${queryParams}`,
+		headers: {
+			'bfx-nonce': nonce,
+			'bfx-apikey': apiKey,
+			'bfx-signature': shex
+		},
+		"content-type" : 'application/json',
+	  	body: body,
+	  	json: true
+	};
+	
+	request.post({
+		uri: uri + "/auth/r/funding/offers/fUSD",
+		headers: {
+		'bfx-nonce': nonce,
+		'bfx-apikey': apiKey,
+		'bfx-signature': shex
+	  	},
+	  }, (error, response, body) => {
+		  console.log(body);
+	});
+*/	
+	/*
+	request.post(options, (error, response, body) => {
+		console.log(body);
+	});
+	*/
+	// const uri = "https://api.bitfinex.com/v1";
+	// console.log("\n");
+	/*
+	const uri = "https://api.bitfinex.com/v1";
+
+	var options = { 
+		method: 'POST',
+	  	url: 'https://api.bitfinex.com/v2/calc/trade/avg',
+	  	qs: { symbol: 'fusd' } };
+		
+		request(options, function (error, response, body) {
+		  if (error) throw new Error(error);
+
+		  console.log(body);
+	});
+	*/
+	/*
+	const fundingbook_options = { 
+		uri: uri + '/lendbook/USD?limit_asks=1', 
+		method: "GET", 
+		timeout: 10000, 
+		followRedirect: true, 
+		maxRedirects: 10
+	};
+		  	
+	request.get(fundingbook_options,
+	  function(error, response, body) {
+		console.log(JSON.stringify(body));
+	});
+	*/
+}
+
+main();
